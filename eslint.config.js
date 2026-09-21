b09b9af228d3674284ce5edc6c0167e387e35ce9
Tanya-Gro <tarpel.gta@gmail.com>
2026-09-21 06:20:22 +0300

    chore: format

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   js.configs.recommended,
+  eslintConfigPrettier,
   { files: ['src/**/*.js'],
     plugins: { js },
     extends: ["js/recommended"],
